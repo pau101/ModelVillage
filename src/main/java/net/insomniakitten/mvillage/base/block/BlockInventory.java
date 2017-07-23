c43988315f89c9fe5a209a1bbbcc9d87061db48f
InsomniaKitten <admin@designdevils.co.uk>
2017-07-23 04:40:53 +0100

    Move block<->container handling to an interface

--- src/main/java/net/insomniakitten/mvillage/base/block/BlockInventory.java
+++ src/main/java/net/insomniakitten/mvillage/base/block/BlockInventory.java
@@ -16,31 +16,23 @@ package net.insomniakitten.mvillage.base.block;
  *   limitations under the License.
  */
 
-import net.insomniakitten.mvillage.ModelVillage;
-import net.insomniakitten.mvillage.base.gui.GuiManager.GuiType;
 import net.insomniakitten.mvillage.base.inventory.InventoryType;
 import net.insomniakitten.mvillage.base.inventory.TileInventory;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.fml.common.network.internal.FMLNetworkHandler;
-import net.minecraftforge.items.IItemHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class BlockInventory extends BlockMV {
+public class BlockInventory extends BlockMV implements IContainer {
 
-    private final static GuiType GUI = GuiType.INVENTORY;
     private InventoryType type;
 
     public BlockInventory(String name, InventoryType type) {
@@ -53,26 +45,13 @@ public class BlockInventory extends BlockMV {
     public boolean onBlockActivated(
             World world, BlockPos pos, IBlockState state, EntityPlayer player,
             EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-        FMLNetworkHandler.openGui(
-                player, ModelVillage.getInstance(), GUI.getID(),
-                    world, pos.getX(), pos.getY(), pos.getZ());
+        handleContainerGui(world, pos, player);
         return true;
     }
 
-    @Override @SuppressWarnings("ConstantConditions")
+    @Override
     public void breakBlock(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
-        Capability<IItemHandler> items = TileInventory.getCapability();
-        TileEntity tile = world.getTileEntity(pos);
-        if (tile != null && tile instanceof TileInventory && tile.hasCapability(items, null)) {
-            IItemHandler inventory = tile.getCapability(items, null);
-            if (inventory != null && world.getGameRules().getBoolean("doTileDrops")) {
-                for (int i = 0; i < inventory.getSlots(); i++) {
-                    ItemStack stack = inventory.getStackInSlot(i);
-                    if (!stack.isEmpty()) InventoryHelper.spawnItemStack(
-                            world, pos.getX(), pos.getY(), pos.getZ(), stack);
-                }
-            }
-        }
+        handleItemDrops(world, pos, state);
     }
 
     @Override
---
+++ src/main/java/net/insomniakitten/mvillage/base/block/IContainer.java
@@ -0,0 +1,58 @@
+package net.insomniakitten.mvillage.base.block;
+
+/*
+ *  Copyright 2017 InsomniaKitten
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+import net.insomniakitten.mvillage.ModelVillage;
+import net.insomniakitten.mvillage.base.gui.GuiManager.GuiType;
+import net.insomniakitten.mvillage.base.inventory.TileInventory;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.fml.common.network.internal.FMLNetworkHandler;
+import net.minecraftforge.items.IItemHandler;
+
+import javax.annotation.Nonnull;
+
+public interface IContainer {
+
+    default void handleContainerGui(World world, BlockPos pos, EntityPlayer player) {
+        FMLNetworkHandler.openGui(
+                player, ModelVillage.getInstance(), GuiType.INVENTORY.getID(),
+                world, pos.getX(), pos.getY(), pos.getZ());
+    }
+
+    default void handleItemDrops(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+        Capability<IItemHandler> items = TileInventory.getCapability();
+        TileEntity tile = world.getTileEntity(pos);
+        if (tile != null && tile instanceof TileInventory && tile.hasCapability(items, null)) {
+            IItemHandler inventory = tile.getCapability(items, null);
+            if (inventory != null && world.getGameRules().getBoolean("doTileDrops")) {
+                for (int i = 0; i < inventory.getSlots(); i++) {
+                    ItemStack stack = inventory.getStackInSlot(i);
+                    if (!stack.isEmpty()) InventoryHelper.spawnItemStack(
+                            world, pos.getX(), pos.getY(), pos.getZ(), stack);
+                }
+            }
+        }
+    }
+
+}
